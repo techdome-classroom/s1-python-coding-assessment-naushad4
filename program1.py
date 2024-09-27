33461491162be172c3d0f383983029abc382f622
Naushad Ali <na864724@gmail.com>
2024-09-27 12:59:31 +0530

    first commit

--- program1.py
+++ program1.py
@@ -1,6 +1,37 @@
 class Solution:
-   
-    def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+    def getTotalIsles(self, grid):
+        if not grid:
+            return 0
+        
+        
+        rows = len(grid)
+        cols = len(grid[0])
+
+    
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+        
+
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        
+        def dfs(r, c):
+            visited[r][c] = True
+            
+            for dr, dc in directions:
+                new_r, new_c = r + dr, c + dc
+
+                if 0 <= new_r < rows and 0 <= new_c < cols and not visited[new_r][new_c] and grid[new_r][new_c] == 'L':
+                    dfs(new_r, new_c)
+        
+        
+        num_islands = 0
+        
+        
+        for r in range(rows):
+            for c in range(cols):
+            
+                if grid[r][c] == 'L' and not visited[r][c]:
+                    num_islands += 1
+                    dfs(r, c)
+        
+        return num_islands
--- program2.py
+++ program2.py
@@ -1,5 +1,31 @@
-def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
+def decode_message(message, pattern):
+    
+    memo = {}
+    
+    
+    def is_match(i, j):
+        
+        if (i, j) in memo:
+            return memo[(i, j)]
+        
+        
+        if j == len(pattern):
+            return i == len(message)
+        
+        
+        if pattern[j] == '*':
+            
+            match = (is_match(i, j + 1) or (i < len(message) and is_match(i + 1, j)))
+            memo[(i, j)] = match
+            return match
+        
+        
+        if i < len(message) and (pattern[j] == '?' or pattern[j] == message[i]):
+            memo[(i, j)] = is_match(i + 1, j + 1)
+            return memo[(i, j)]
+        
+        
+        memo[(i, j)] = False
         return False
+    
+    return is_match(0, 0)
